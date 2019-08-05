dce8a01828aaf5f3d11967661e3c1d02266ef737
ElOskar101 <oscaresga22@gmail.com>
2019-08-04 18:09:15 -0600

    Adding basically views | My left right eye is tearing up | First API method.

---
+++ Sisuni/Controllers/SearchController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Data;
+using Data.ViewModels;
+
+namespace Sisuni.Controllers{
+    public class SearchController : Controller{
+        // GET: Search
+        public ActionResult Index(){
+            return View();
+        }
+
+        public ActionResult Search(string section) {
+            /*using (var db = new Context()) {
+
+                var list = db.CourseStudent.ToList();
+                return View(list);
+            }*/
+            return View();
+        }
+    }
+
+    
+}
